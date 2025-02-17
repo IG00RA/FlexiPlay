5296fb17e429cd34366a5e408975527b3d30187e
Igor Gromadskiy <gromadskiyigor@gmail.com>
2025-02-17 21:25:56 +0200

    [Add] main page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,29 +1,9 @@
-import About from '@/components/About/About';
-import Achieve from '@/components/Achieve/Achieve';
-import Clients from '@/components/Clients/Clients';
-import Community from '@/components/Community/Community';
-import Contacts from '@/components/Contacts/Contacts';
-import Development from '@/components/Development/Development';
-import Ecosystem from '@/components/Ecosystem/Ecosystem';
-import Future from '@/components/Future/Future';
-import Hero from '@/components/Hero/Hero';
-import Media from '@/components/Media/Media';
-import Values from '@/components/Values/Values';
+import MainPage from "@/components/MainPage/MainPage";
 
 export default function Home() {
   return (
     <>
-      {/* <Hero />
-      <About />
-      <Ecosystem />
-      <Development />
-      <Community />
-      <Media />
-      <Values />
-      <Achieve />
-      <Clients />
-      <Future />
-      <Contacts /> */}
+      <MainPage />
     </>
   );
 }
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,62 @@
-// import store from '../img/development/store.webp';
+import board from '../img/main/board.webp';
+import color from '../img/main/color.webp';
+import combinations from '../img/main/combinations.webp';
+import introduction from '../img/main/introduction.webp';
+import memory from '../img/main/memory.webp';
+import placing from '../img/main/placing.webp';
+import board_mob from '../img/main/board_mob.webp';
+import color_mob from '../img/main/color_mob.webp';
+import combinations_mob from '../img/main/combinations_mob.webp';
+import introduction_mob from '../img/main/introduction_mob.webp';
+import memory_mob from '../img/main/memory_mob.webp';
+import placing_mob from '../img/main/placing_mob.webp';
 
-export const navItems = [
-  { label: 'Header.about', href: '#about' },
+export const navItems = [{ label: 'Header.about', href: '#about' }];
+
+export const mainPageGeometry = [
+  {
+    img: memory,
+    imgMob: memory_mob,
+    header: 'MainPage.memory.header',
+    description: 'MainPage.memory.description',
+    time: '10-15 ',
+  },
+  {
+    img: board,
+    imgMob: board_mob,
+    header: 'MainPage.board.header',
+    description: 'MainPage.board.description',
+    time: '8-12 ',
+  },
+];
+
+export const mainPageVideo = [
+  {
+    img: introduction,
+    imgMob: introduction_mob,
+    header: 'MainPage.introduction.header',
+    description: 'MainPage.introduction.description',
+    time: '10-15 ',
+  },
+  {
+    img: color,
+    imgMob: color_mob,
+    header: 'MainPage.color.header',
+    description: 'MainPage.color.description',
+    time: '8-12 ',
+  },
+  {
+    img: placing,
+    imgMob: placing_mob,
+    header: 'MainPage.placing.header',
+    description: 'MainPage.placing.description',
+    time: '12-15 ',
+  },
+  {
+    img: combinations,
+    imgMob: combinations_mob,
+    header: 'MainPage.combinations.header',
+    description: 'MainPage.combinations.description',
+    time: '10-12 ',
+  },
 ];
