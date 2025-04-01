7568a962598d85c3ff4238569aa144f342fd67f1
Igor Gromadskiy <gromadskiyigor@gmail.com>
2025-04-01 13:07:28 +0300

    [Add] locale redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,27 @@
 import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import { routing } from '@/i18n/routing';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware(routing);
+function getLocaleFromRequest(request: NextRequest) {
+  const country = request.headers.get('x-country-code');
+
+  if (country === 'SK' || country === 'SI') return 'sk';
+  return 'uk';
+}
+
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname === '/') {
+    const locale = getLocaleFromRequest(request);
+    return NextResponse.redirect(new URL(`/${locale}`, request.url));
+  }
+
+  return intlMiddleware(request);
+}
 
 export const config = {
-  matcher: ['/', '/(uk|sk)/:path*'], // At this line, define into the matcher all the availables language you have defined into routing.ts
+  matcher: ['/', '/(uk|sk)/:path*'],
 };
